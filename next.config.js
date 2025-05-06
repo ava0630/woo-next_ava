903d2f4338e22b1a753cce74cfdd05996dbcf29b
ava0630 <ava0630ava@gmail.com>
2025-05-06 17:02:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 const path = require("path");
 const allowedImageWordPressDomain = new URL(process.env.NEXT_PUBLIC_WORDPRESS_URL).hostname
 
-module.exports = {
-    trailingSlash: true,
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-        };
 
-        return config;
-    },
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
     },
