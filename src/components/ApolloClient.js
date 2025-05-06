40c0c281e23c1114c8df658155ddff6981532e1f
ava0630 <ava0630ava@gmail.com>
2025-05-06 16:57:34 +0800

    Update ApolloClient.js

--- src/components/ApolloClient.js
+++ src/components/ApolloClient.js
@@ -65,9 +65,7 @@ export const afterware = new ApolloLink( ( operation, forward ) => {
 
 // Apollo GraphQL client.
 const client = new ApolloClient({
-	link: middleware.concat( afterware.concat( createHttpLink({
-		uri: `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql`,
-		fetch: fetch
+	link:httpLink,
 	}) ) ),
 	cache: new InMemoryCache(),
 });
