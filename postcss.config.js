1c439699a481f97ccd4f70a7fba39a9bc92958d7
ava0630 <ava0630ava@gmail.com>
2025-05-07 02:30:17 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  plugins: {
-    "postcss-import": {}, 
-    "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {
+  plugins: [
+    "postcss-import", 
+    "postcss-flexbugs-fixes",
+    ["postcss-preset-env", { 
       autoprefixer: { flexbox: "no-2009" },
       stage: 3,
       features: { "custom-properties": false }
-    },
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+    }],
+    "tailwindcss",
+    "autoprefixer"
+  ]
 }
