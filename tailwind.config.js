5130762f68de398a32708c3509564d2f607ef3e6
ava0630 <ava0630ava@gmail.com>
2025-05-07 00:45:01 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx}",
-      "./src/components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
